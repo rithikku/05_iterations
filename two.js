7427e7e01c4c871da6b7960fc349270dce71bd51
Rithik Kumar <rithikarchadefacilator@gmail.com>
2025-02-02 14:45:50 +0530

    1st Commit

---
+++ two.js
@@ -0,0 +1,23 @@
+let i = 0
+while (i < 10) {
+    // console.log(`Value of i is ${i}`);
+    i = i + 2;
+}
+
+
+let myArray = ["flash", "batman", "superman"];
+
+let arr = 0;
+while (arr < myArray.length) {
+    // console.log(myArray[arr]);
+    arr = arr + 1;
+}
+
+
+// in this loop -- kaam pehle hoga fir condition check hoga 
+let score = 1;
+
+do {
+    console.log(`Score is ${score}`);
+    score ++;
+} while (score <= 10);
